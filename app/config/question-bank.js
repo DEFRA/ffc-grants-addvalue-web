e825383a8f31d68e89c04055809962220a609085
diya2214 <madihakhn@yahoo.com>
2021-12-20 18:20:08 +0000

    cost fix

--- app/config/question-bank.js
+++ app/config/question-bank.js
@@ -740,7 +740,7 @@ const questionBank = {
           id: 'projectCost',
           name: 'projectCost',
           prefix: { text: '£' },
-          type: 'number',
+          type: 'cost',
           grantInfo: {
             minGrant: 35000,
             maxGrant: 500000,
