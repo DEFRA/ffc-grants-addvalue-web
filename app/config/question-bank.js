b20cc0777fabc645bf2c9021daebca12f8688b79
Hozan94 <hozan949@gmail.com>
2024-05-20 11:29:28 +0100

    chore: fix validation errror

--- app/config/question-bank.js
+++ app/config/question-bank.js
@@ -1151,7 +1151,7 @@ const questionBank = {
           validate: [
             {
               type: 'NOT_EMPTY',
-              error: 'Select the type of produce being processed'
+              error: 'Select what type of produce is being processed'
             }
           ],
           answers: [
