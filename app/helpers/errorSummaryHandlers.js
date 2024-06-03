6064eb42171e75f6ce1f8d36854a1495ce5079d3
Hozan94 <hozan949@gmail.com>
2024-06-03 15:48:53 +0100

    chore: refactore some code for sonar

--- app/helpers/errorSummaryHandlers.js
+++ app/helpers/errorSummaryHandlers.js
@@ -109,17 +109,14 @@ const checkErrors = (payload, currentQuestion, h, request) => {
               errorText = "Select either 'Starting to make added-value products for the first time' or 'Increasing range'"
             } else if (index === 2 || index === 5) {
               errorText = "Select either 'Starting to make added-value products for the first time' or 'Increasing volume' and 'Increasing range'"
-            }
+            } 
+            errorHrefList.push({
+              text: errorText,
+              href: `#${placeholderInputError.dependentKey ?? yarKey}`
+            })
           }
         }
       })
-
-      if (errorText) {
-        errorHrefList.push({
-          text: errorText,
-          href: `#${placeholderInputError.dependentKey ?? yarKey}`
-        })
-      }
 
     } else if (placeholderInputError) {
       errorHrefList.push({
