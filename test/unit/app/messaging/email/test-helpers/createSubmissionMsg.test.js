acf43e395e3db2751b455cf30b85451b72b491f6
Yunus FIRAT <yunusfirat44@gmail.com>
2024-06-04 10:57:44 +0100

    update..

--- test/unit/app/messaging/email/test-helpers/createSubmissionMsg.test.js
+++ test/unit/app/messaging/email/test-helpers/createSubmissionMsg.test.js
@@ -49,7 +49,7 @@ describe('Create submission message', () => {
     expect(msg).toHaveProperty('applicantEmail')
     expect(msg).toHaveProperty('rpaEmail')
     expect(msg).toHaveProperty('spreadsheet')
-    expect(msg.applicantEmail.emailAddress).toBe(farmerSubmission.farmerDetails.emailAddress)
+    expect(msg.applicantEmail.emailAddress).toBe(fruitStorageSubmission.farmerDetails.emailAddress)
     expect(msg.rpaEmail.emailAddress).toBe('FTF@rpa.gov.uk')
     expect(msg.agentEmail).toBe(null)
   })
