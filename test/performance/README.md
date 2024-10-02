90548c580c7465f0de5f5dff235f777e77f3e499
Andrew Deacon <andrew.deacon@equalexperts.com>
2024-10-02 18:19:37 +0100

    Initial k6 test

---
+++ test/performance/README.md
@@ -0,0 +1,12 @@
+# Performance Tests
+This folder contains the performance tests for the Adding Value Grant web app. The framework used is Grafana k6.
+
+## Requirements
+- Docker
+- npm
+- k6 (if running outside a container)
+
+## Running tests outside a container
+```pwsh
+k6 run script.js
+```
---
+++ test/performance/script.js
@@ -0,0 +1,115 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 50,
+    duration: '2m',
+    thresholds: {
+      http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+      http_req_duration: ['p(95)<1500'], // 95% of requests should be below 1500ms
+    }
+};
+
+export default function () {
+    let response = null;
+    let crumb = null;
+
+    const submitJourneyForm = function(fields) {
+        sleep(2); // mimic human behaviour
+        let fieldsWithCrumb = { crumb: crumb };
+        if (fields) {
+            for (const [key, value] of Object.entries(fields)) {
+                fieldsWithCrumb[key] = value;
+            }
+        }
+        response = response.submitForm({ formSelector: `form[method='POST']`, fields: fieldsWithCrumb });
+        check(response, { 'is status 200': (r) => r.status === 200});
+    }
+
+    const followLinkWithText = function(text) {
+        response = response.clickLink({ selector: `a:contains('${text}')` });
+    }
+
+    response = http.get('https://ffc-grants-frontend-test.azure.defra.cloud/adding-value/start');
+
+    // start
+    if (response.url.endsWith('login')) {
+        response = response.submitForm({ fields: { username: 'grants', password: 'grants2021' }});
+    }
+    followLinkWithText('Start now');
+    crumb = response.html().find('#crumb').attr('value');
+
+    // nature-of-business
+    submitJourneyForm({ applicantBusiness: 'applicantBusiness' });
+    // legal-status
+    submitJourneyForm({ legalStatus: 'Sole trader' });    
+    // country
+    submitJourneyForm({ inEngland: 'Yes' });
+    // planning-permission
+    submitJourneyForm({ planningPermission: 'Secured' });
+    // project-start
+    submitJourneyForm({ projectStart: 'No, we have not done any work on this project yet'} );
+    // tenancy
+    submitJourneyForm({ tenancy: 'Yes'});
+    // smaller-abattoir
+    submitJourneyForm({ smallerAbattoir: 'Yes'});
+    // other-farmers
+    submitJourneyForm({ otherFarmers: 'Yes' });
+    // project-items
+    submitJourneyForm({ projectItems: 'Constructing or improving buildings for processing' });
+    // storage
+    submitJourneyForm({ storage: 'Yes, we will need storage facilities' });
+    // solar-PV-system
+    submitJourneyForm({ solarPVSystem: 'Yes' });
+    // project-cost
+    submitJourneyForm({ projectCost: '100000' });
+    // solar-PV-cost
+    submitJourneyForm({ solarPVCost: '50000' });
+    // potential-amount-solar
+    followLinkWithText('Continue');
+    // remaining-costs
+    submitJourneyForm({ canPayRemainingCost: 'Yes' });
+    // produce-processed
+    submitJourneyForm({ productsProcessed: 'Wild venison meat produce' });
+    // how-adding-value
+    submitJourneyForm({ howAddingValue: 'Introducing a new product to your farm' });
+    // project-impact
+    submitJourneyForm({ projectImpact: ['Increasing range of added-value products', 'Increasing volume of added-value products'] });
+    // mechanisation
+    submitJourneyForm({ mechanisation: 'Yes' });
+    // manual-labour-amount
+    submitJourneyForm({ manualLabour: 'More than 10%' });
+    // future-customers
+    submitJourneyForm({ futureCustomers: ['Processors', 'Wholesalers'] });
+    // collaboration
+    submitJourneyForm({ collaboration: 'Yes' });
+    // environmental-impact
+    submitJourneyForm({ environmentalImpact: ['Renewable energy', 'Energy efficiency'] });
+    // score
+    submitJourneyForm();
+    // business-details
+    submitJourneyForm({ projectName: 'Smaller Abattoir Project', businessName: 'Test Farm Ltd', numberEmployees: '5', businessTurnover: '2000000', sbi: '123456789' });
+    // applying
+    submitJourneyForm({ applying: 'Applicant' });
+    // applicant-details
+    submitJourneyForm({
+        firstName: 'James',
+        lastName: 'Test-Farmer',
+        emailAddress: 'cl-defra-tactical-grants-test-applicant-email@equalexperts.com',
+        confirmEmailAddress: 'cl-defra-tactical-grants-test-applicant-email@equalexperts.com',
+        mobileNumber: '07777 123456',
+        landlineNumber: '01604 123456',
+        address1: 'Test Farm',
+        address2: 'Cogenhoe',
+        town: 'Northampton',
+        county: 'Northamptonshire',
+        postcode: 'NN7 1NN',
+        projectPostcode: 'NN7 2NN'
+     });
+     // check-details
+     submitJourneyForm();
+     // confirm
+     submitJourneyForm();
+
+    console.warn("URL is: " + response.url);
+}
